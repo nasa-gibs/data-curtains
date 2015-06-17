098a51f177ee8430a3398a7a725403c3b277f76e
Abhishek Potnis <abhishekp.bugzilla@gmail.com>
2015-06-17 20:55:38 +0530

    Script for extracting meta-data using ECHO API

---
+++ getMetaData/index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<center>
+<form method = "POST" action = "process.php">
+<input name = "date" type="date"/>
+<input type = "submit" value = "Submit"/>
+</form>
+</center>
+</html>
+
