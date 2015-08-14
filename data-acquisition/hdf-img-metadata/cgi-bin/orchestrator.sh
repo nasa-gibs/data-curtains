214e851a81da7fbdd0fea4d58a578de70230c42e
Abhishek Potnis <abhishekp.bugzilla@gmail.com>
2015-08-14 16:11:37 +0530

    Fixed misinterpretation of Day-Night Flag from .hdf file

--- data-acquisition/hdf-img-metadata/cgi-bin/orchestrator.sh
+++ data-acquisition/hdf-img-metadata/cgi-bin/orchestrator.sh
@@ -97,32 +97,33 @@ cgi_getvars BOTH ALL
 echo "Selected Date = "$date
 
 i=0
-s1='img'
-s2='meta'
-if [ "$img" = "$s1" ];
-then
-	echo "Extracting Imagery..."
-	for file in "$HDF_DIR"/CAL_LID_L1-ValStage1-V3-30.$date*
-	do
-		python extractImagery.py $file
-	done
-else
-echo "Imagery Extraction Not Selected"
-fi
+#s1='img'
+#s2='meta'
+#if [ "$img" = "$s1" ];
+#then
+echo "Extracting Imagery..."
+for file in "$HDF_DIR"/CAL_LID_L1-ValStage1-V3-30.$date*
+do
+	python extractImagery.py $file
+done
+#else
+#echo "Imagery Extraction Not Selected"
+#fi
 
-if [ "$meta" = "$s2" ];
-then
-	echo "Extracting Metadata..."
-	for file in "$HDF_DIR"/CAL_LID_L1-ValStage1-V3-30.$date*
-	do
-		echo "$i"
-		hdfs["$i"]=$file
-		((i=i+1))
-		python extractImagery.py $file
-	done
+#if [ "$meta" = "$s2" ];
+#then
+echo "Extracting Metadata..."
+for file in "$HDF_DIR"/CAL_LID_L1-ValStage1-V3-30.$date*
+do
+#		echo "$i"
+	hdfs["$i"]=$file
+	((i=i+1))
+	python extractImagery.py $file
+done
+
 	python extractMetadata.py ${hdfs[@]}
-else
+#else
 echo "Metadata Extraction Not Selected"
-fi
+#fi
 
-echo "Done!"
+#echo "Done!"
